7f1abb9852ed451506b818ca91f1918a5bb5f802
Popov85 <dT09Rx06>
2016-10-09 23:07:51 +0300

    Initial commit

---
+++ src/main/java/com/goit/popov/ee07/TestJDBC.java
@@ -0,0 +1,43 @@
+package com.goit.popov.ee07;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+/**
+ * Created by Andrey on 10/9/2016.
+ */
+public class TestJDBC {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(TestJDBC.class);
+        public static void main(String[] args) {
+                loadDriver();
+                LOGGER.info("Connecting to database....");
+                String url = "jdbc:postgresql://localhost:5432/company";
+                String user = "postgres";
+                String password = "dT09Rx06";
+                try (Connection connection = DriverManager.getConnection(url, user, password);
+                        Statement statement = connection.createStatement()){
+                        String sql = "SELECT * from employee where age=30";
+                        ResultSet resultSet = statement.executeQuery(sql);
+                        while (resultSet.next()) {
+                                System.out.println("id: "+resultSet.getInt("ID"));
+                        }
+                       LOGGER.info("Connected successfully!");
+                } catch (SQLException e) {
+                        LOGGER.error("Exception occurred!");
+                }
+        }
+
+        private static void loadDriver() {
+                try {
+                        LOGGER.info("...loading jdbc driver");
+                        Class.forName("org.postgresql.Driver");
+                        LOGGER.info("Successfully loaded!");
+                } catch (ClassNotFoundException e) {
+                        LOGGER.error("Cannot find driver");
+                        throw new RuntimeException(e);
+                }
+        }
+}
