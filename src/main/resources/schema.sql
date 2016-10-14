be30efcb805e33134237b3ab84863fe43a28cc9c
Popov85 <dT09Rx06>
2016-10-14 15:36:07 +0300

    Optimized schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,79 +1,79 @@
-CREATE TABLE IF NOT EXISTS position (
+CREATE TABLE "position" (
   id INT NOT NULL,
-  title VARCHAR(50) NOT NULL,
+  title VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS employee (
+CREATE TABLE employee (
   id INT NOT NULL,
   name VARCHAR(50) NOT NULL,
   dob DATE,
-  phone VARCHAR (20),
-  position_id INT,
+  phone VARCHAR (20) UNIQUE,
+  position_id INT NOT NULL,
   salary REAl,
   PRIMARY KEY (id),
   FOREIGN KEY (position_id)
-  REFERENCES "position" (id)
-  );
+  REFERENCES "position" (id));
 
-CREATE TABLE IF NOT EXISTS ingredient (
+CREATE TABLE ingredient (
   id INT NOT NULL,
-  ingredient_name VARCHAR(50) NOT NULL,
+  ingredient_name VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS dish (
+CREATE TABLE dish (
   id INT NOT NULL,
-  dish_name VARCHAR(50) NOT NULL,
+  dish_name VARCHAR(50) NOT NULL UNIQUE,
   category VARCHAR (50),
   price REAL,
   weight REAL,
-  PRIMARY KEY (id)
-  );
+  PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS dish_ingredient (
-  id INT NOT NULL,
+CREATE TABLE dish_ingredient (
   dish_id int,
   ingredient_id int,
-  PRIMARY KEY (id),
   FOREIGN KEY (dish_id)
   REFERENCES "dish" (id),
   FOREIGN KEY (ingredient_id)
-  REFERENCES "ingredient" (id)
-  );
+  REFERENCES "ingredient" (id));
 
-CREATE TABLE IF NOT EXISTS order_ (
+CREATE TABLE "order" (
   id INT NOT NULL,
   order_date TIMESTAMP,
   table_number int,
   employee_id int,
-  dish_id int,
   PRIMARY KEY (id),
   FOREIGN KEY (employee_id)
-  REFERENCES "employee" (id),
-  FOREIGN KEY (dish_id)
-  REFERENCES "dish" (id)
-  );
+  REFERENCES "employee" (id));
 
-CREATE TABLE IF NOT EXISTS menu (
-  id INT NOT NULL,
-  menu_name VARCHAR(50) NOT NULL,
-  dish_id int,
-  PRIMARY KEY (id),
+  CREATE TABLE order_dish (
+  order_id INT NOT NULL,
+  dish_id INT NOT NULL,
+  FOREIGN KEY (order_id)
+  REFERENCES "order" (id),
   FOREIGN KEY (dish_id)
-  REFERENCES "dish" (id)
-  );
+  REFERENCES "dish" (id));
 
-CREATE TABLE IF NOT EXISTS store_house (
+CREATE TABLE menu (
   id INT NOT NULL,
+  menu_name VARCHAR(50) NOT NULL UNIQUE,
+  PRIMARY KEY (id));
+
+CREATE TABLE menu_structure (
+  menu_id INT NOT NULL,
+  dish_id INT NOT NULL,
+  FOREIGN KEY (menu_id)
+  REFERENCES "menu" (id),
+  FOREIGN KEY (dish_id)
+  REFERENCES "dish" (id));
+
+CREATE TABLE store_house (
   ingredient_id int NOT NULL,
-  quantiity int,
-  PRIMARY KEY (id),
+  quantiity char(30),
   FOREIGN KEY (ingredient_id)
-  REFERENCES "ingredient" (id)
-  );
+  REFERENCES "ingredient" (id));
 
-CREATE TABLE IF NOT EXISTS prepared_dishes (
+CREATE TABLE prepared_dishes (
   id INT NOT NULL,
-  order_date TIMESTAMP,
+  prepared_dish TIMESTAMP,
   dish_id int,
   employee_id int,
   order_id int,
@@ -83,5 +83,4 @@ CREATE TABLE IF NOT EXISTS prepared_dishes (
   FOREIGN KEY (employee_id)
   REFERENCES "employee" (id),
   FOREIGN KEY (order_id)
-  REFERENCES "order_" (id)
-  );
+  REFERENCES "order" (id));
